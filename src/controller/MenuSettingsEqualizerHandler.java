0080c3e26453ab896fd64e4560d98376ed6fee3a
pmma_ <laurentSpam99@gmail.com>
2020-01-28 22:23:10 +0100

    Added a little style to equalizer

--- src/controller/MenuSettingsEqualizerHandler.java
+++ src/controller/MenuSettingsEqualizerHandler.java
@@ -3,8 +3,11 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import ressource.Data;
 import ressource.References;
+import sun.applet.Main;
 import view.EqualizerPane;
 
 public class MenuSettingsEqualizerHandler implements EventHandler<ActionEvent> {
@@ -12,10 +15,14 @@ public class MenuSettingsEqualizerHandler implements EventHandler<ActionEvent> {
 	@Override
 	public void handle(ActionEvent e) {
 		EqualizerPane root = new EqualizerPane();
-		Scene scene = new Scene(root, 400, 600);
+		Scene scene = new Scene(root, 500, 300);
 		Stage stage = new Stage();
 		
+		scene.getStylesheets().add(EqualizerPane.class.getResource("css/application.css").toExternalForm());
+		
 		stage.setTitle("Equalizer");
+		stage.getIcons().add(new Image(Data.class.getResourceAsStream("img/equalizer.png")));
+		stage.setResizable(false);
 		stage.setScene(scene);
 		stage.show();
 	}
