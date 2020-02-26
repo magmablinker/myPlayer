e2dfe33fb8859c3fa1da4345bc5282bf8904cf1a
pmma_ <laurentSpam99@gmail.com>
2020-02-26 20:20:59 +0100

    yes

--- src/model/Database.java
+++ src/model/Database.java
@@ -26,7 +26,6 @@ public class Database {
 			Class.forName("com.mysql.jdbc.Driver");
 			this.conn = DriverManager.getConnection(database, username, password);
 		} catch(Exception e) {
-			e.printStackTrace();
 			Alert alert = new Alert(AlertType.ERROR, "The database connection failed. " + e.toString());
 			DialogPane dialogPane = alert.getDialogPane();
 			dialogPane.getStylesheets().add(Main.class.getResource("css/application.css").toExternalForm());
